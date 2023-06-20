472a1e5c57ee684bbb1aaa377f328c3cf306ce88
Дмитрий Бейкун <alexkomkov39@gmail.com>
2023-06-21 01:44:27 +0200

    commit all

--- QuizAppProj/Autorization/LoginForm.xaml.cs
+++ QuizAppProj/Autorization/LoginForm.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +22,46 @@ namespace QuizAppProj.Autorization
     /// </summary>
     public partial class LoginForm : Page
     {
+        private ConnectToDatabase connect = new ConnectToDatabase();
         public LoginForm()
         {
             InitializeComponent();
         }
         private void AutorizeEventArg(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new MainPage());
+            var loginUser = loginTextBox.Text;
+            var passwordUser = passwordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(loginUser) || string.IsNullOrWhiteSpace(passwordUser))
+            {
+                MessageBox.Show("Введите логин и пароль.", "Что - то не так...", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-HCK9T1F\SQLEXPRESS;Initial Catalog=QuizDB;Integrated Security=True");
+
+            connection.Open();
+
+            string query = "SELECT COUNT(*) FROM Users WHERE login = @Login AND password = @Password";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@Login", loginUser);
+            command.Parameters.AddWithValue("@Password", passwordUser);
+
+            int count = (int)command.ExecuteScalar();
+
+            if (count > 0)
+            {
+                MessageBox.Show("Авторизация прошла успешно.", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                NavigationService.Navigate(new MainPage());
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.", "Что - то не так...", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            connection.Close();
         }
     }
 }
