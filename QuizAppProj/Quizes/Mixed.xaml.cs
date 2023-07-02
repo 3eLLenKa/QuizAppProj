354658932f78ce9b7e8fefdfb501a409af9bbfb1
Дмитрий Бейкун <alexkomkov39@gmail.com>
2023-07-02 02:07:47 +0200

    commit all

--- QuizAppProj/Quizes/Mixed.xaml.cs
+++ QuizAppProj/Quizes/Mixed.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuizAppProj.Autorization;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,238 @@ namespace QuizAppProj.Quizes
     /// </summary>
     public partial class Mixed : Page
     {
+        private List<CheckBox> checkBoxes;
+        private List<CheckBox> customCheckedBoxes = new List<CheckBox>(3);
+
+        protected static HashSet<string> settings = new HashSet<string>();
         public Mixed()
         {
             InitializeComponent();
-        }
 
+            checkBoxes = new List<CheckBox>()
+            {
+                { easyQuestionsCheckBox },
+                { normalQuestionsCheckBox },
+                { hardQuestionsCheckBox },
+                { easyTimeCheckBox },
+                { normalTimeCheckBox },
+                { hardTimeCheckBox },
+                { easyCountCheckBox },
+                { normalCountCheckBox },
+                { hardCountCheckBox }
+            };
+        }
         private void GoBack(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void BiologyQuizNavigation(object sender, RoutedEventArgs e)
+        {
+            if (settings.Count < 3) { MessageBox.Show("Вы выбрали не все настройки!"); return; }
+            else NavigationService.Navigate(new MixedQuiz());
+        }
+
+        //Настройки сложности вопросов
+
+        private void easyQuestionsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("easyQuestionsCheckBox");
+
+            normalQuestionsCheckBox.IsChecked = false;
+            hardQuestionsCheckBox.IsChecked = false;
+        }
+
+        private void normalQuestionsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("normalQuestionsCheckBox");
+
+            easyQuestionsCheckBox.IsChecked = false;
+            hardQuestionsCheckBox.IsChecked = false;
+        }
+
+        private void hardQuestionsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("hardQuestionsCheckBox");
+
+            easyQuestionsCheckBox.IsChecked = false;
+            normalQuestionsCheckBox.IsChecked = false;
+        }
+
+        //Настройки кол-ва времени на ответ
+
+        private void easyTimeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("easyTimeCheckBox");
+
+            normalTimeCheckBox.IsChecked = false;
+            hardTimeCheckBox.IsChecked = false;
+        }
+
+        private void normalTimeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("normalTimeCheckBox");
+
+            easyTimeCheckBox.IsChecked = false;
+            hardTimeCheckBox.IsChecked = false;
+        }
+
+        private void hardTimeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("hardTimeCheckBox");
+
+            easyTimeCheckBox.IsChecked = false;
+            normalTimeCheckBox.IsChecked = false;
+        }
+
+        //Настройки кол-ва вопросов
+
+        private void easyCountCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("easyCountCheckBox");
+
+            normalCountCheckBox.IsChecked = false;
+            hardCountCheckBox.IsChecked = false;
+        }
+
+        private void normalCountCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("normalCountCheckBox");
+
+            easyCountCheckBox.IsChecked = false;
+            hardCountCheckBox.IsChecked = false;
+        }
+
+        private void hardCountCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            settings.Add("hardCountCheckBox");
+
+            easyCountCheckBox.IsChecked = false;
+            normalCountCheckBox.IsChecked = false;
+        }
+
+        //Пресеты сложности
+
+        private void easyPresrt_Checked(object sender, RoutedEventArgs e)
+        {
+            saveSettings.Visibility = Visibility.Hidden;
+
+            settings.Clear();
+
+            settings.Add("easyQuestionsCheckBox");
+            settings.Add("easyTimeCheckBox");
+            settings.Add("easyCountCheckBox");
+
+            easyQuestionsCheckBox.IsChecked = true;
+            easyTimeCheckBox.IsChecked = true;
+            easyCountCheckBox.IsChecked = true;
+        }
+
+        private void normalPresrt_Checked(object sender, RoutedEventArgs e)
+        {
+            saveSettings.Visibility = Visibility.Hidden;
+
+            settings.Clear();
+
+            settings.Add("normalQuestionsCheckBox");
+            settings.Add("normalTimeCheckBox");
+            settings.Add("normalCountCheckBox");
+
+            normalQuestionsCheckBox.IsChecked = true;
+            normalTimeCheckBox.IsChecked = true;
+            normalCountCheckBox.IsChecked = true;
+        }
+
+        private void hardPresrt_Checked(object sender, RoutedEventArgs e)
+        {
+            saveSettings.Visibility = Visibility.Hidden;
+
+            settings.Clear();
+
+            settings.Add("hardQuestionsCheckBox");
+            settings.Add("hardTimeCheckBox");
+            settings.Add("hardCountCheckBox");
+
+            hardQuestionsCheckBox.IsChecked = true;
+            hardTimeCheckBox.IsChecked = true;
+            hardCountCheckBox.IsChecked = true;
+        }
+
+        private void customPresrt_Checked(object sender, RoutedEventArgs e)
+        {
+
+            saveSettings.Visibility = Visibility.Visible;
+
+            settings.Clear();
+
+            SessionCheckUtilities utilities = new SessionCheckUtilities();
+            string uid = utilities.ReadUID();
+
+            SqlConnection connection = new SqlConnection(utilities.ConnectionString);
+
+            connection.Open();
+
+            string query = $"SELECT mixed_questions_setting, mixed_time_setting, mixed_count_setting FROM Users WHERE id = {uid}";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+
+            settings.Add(reader[0].ToString());
+            settings.Add(reader[1].ToString());
+            settings.Add(reader[2].ToString());
+
+            reader.Close();
+            connection.Close();
+
+            foreach (CheckBox item in checkBoxes)
+            {
+                if (settings.Contains(item.Name))
+                {
+                    item.IsChecked = true;
+                }
+            }
+        }
+
+        private void saveSettingsClick(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+
+            for (int i = 0; i < checkBoxes.Count; i++)
+            {
+                if (checkBoxes[i].IsChecked == true)
+                {
+                    customCheckedBoxes.Add(checkBoxes[i]);
+                    count++;
+                }
+            }
+
+            if (count == 3)
+            {
+                SessionCheckUtilities utilities = new SessionCheckUtilities();
+                string uid = utilities.ReadUID();
+
+                SqlConnection connection = new SqlConnection(utilities.ConnectionString);
+                connection.Open();
+
+                string query = "UPDATE Users SET mixed_questions_setting = @Questions, mixed_time_setting = @Time, mixed_count_setting = @Count WHERE id = @UID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@UID", uid);
+                command.Parameters.AddWithValue("@Questions", customCheckedBoxes[0].Name);
+                command.Parameters.AddWithValue("@Time", customCheckedBoxes[1].Name);
+                command.Parameters.AddWithValue("@Count", customCheckedBoxes[2].Name);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Ваши настройки сохранены!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                customCheckedBoxes.Clear();
+            }
+            else { MessageBox.Show("Вы выбрали не все настройки!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); customCheckedBoxes.Clear(); }
         }
     }
 }
---
+++ QuizAppProj/Quizes/MixedQuiz.xaml.cs
@@ -0,0 +1,213 @@
+﻿using QuizAppProj.Autorization;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace QuizAppProj.Quizes
+{
+    /// <summary>
+    /// Логика взаимодействия для MixedQuiz.xaml
+    /// </summary>
+    public partial class MixedQuiz : Page
+    {
+        private MixedSettings settings = new MixedSettings();
+
+        private List<RadioButton> radioButtons;
+
+        private DispatcherTimer timer;
+
+        private int secondsElapsed;
+        private int points;
+        private int answersOffset;
+        private int maxCount;
+
+        private int numberQuestion = 1;
+        private int textNumberQuestion = 0;
+
+        public MixedQuiz()
+        {
+            InitializeComponent();
+
+            radioButtons = new List<RadioButton>() { { firstAnswer }, { secondAnswer }, { thirdAnswer }, { fourthAnswer } };
+            maxCount = settings.MaxCount;
+            points = 0;
+
+            InitializeQuiz();
+        }
+
+        private void InitializeQuiz()
+        {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = settings.MaxCount;
+
+            pointsTextBox.Text = $"Кол-во баллов: {points}";
+
+            questionNumberTextBox.Text = $"Вопрос #{numberQuestion}";
+            questionTextBox.Text = settings.gameQuestions.ElementAt(textNumberQuestion).Value;
+
+            for (int i = 0; i < radioButtons.Count; i++)
+            {
+                radioButtons[i].Content = settings.gameAnswers.ElementAt(i);
+            }
+
+            StartTimer();
+        }
+
+        private void AnswerButtonClick(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < radioButtons.Count; i++)
+            {
+                if (radioButtons[i].IsChecked == true & radioButtons[i].Content.ToString() == settings.gameQuestions.ElementAt(textNumberQuestion).Key)
+                {
+                    timer.Stop();
+
+                    points += settings.MaxPoints;
+                    pointsTextBox.Text = $"Кол-во баллов: {points}";
+
+                    radioButtons[i].Background = Brushes.LimeGreen;
+                    radioButtons[i].Foreground = Brushes.Lime;
+
+                    answerButton.Visibility = Visibility.Hidden;
+                    continueButton.Visibility = Visibility.Visible;
+
+                    isFinish();
+
+                    break;
+                }
+
+                if (radioButtons[i].IsChecked == true & radioButtons[i].Content.ToString() != settings.gameQuestions.ElementAt(textNumberQuestion).Key)
+                {
+                    timer.Stop();
+
+                    foreach (var item in radioButtons)
+                    {
+                        if (settings.gameQuestions.ContainsKey(item.Content.ToString()))
+                        {
+                            item.Foreground = Brushes.LimeGreen;
+                            break;
+                        }
+                    }
+
+                    radioButtons[i].Background = Brushes.Red;
+                    radioButtons[i].Foreground = Brushes.Red;
+
+                    answerButton.Visibility = Visibility.Hidden;
+                    continueButton.Visibility = Visibility.Visible;
+
+                    isFinish();
+
+                    break;
+                }
+            }
+        }
+
+        private void isFinish()
+        {
+            if (maxCount == 0)
+            {
+                continueButton.Visibility = Visibility.Hidden;
+                answerButton.Visibility = Visibility.Hidden;
+
+                SaveResult();
+
+                MessageBox.Show($"Вы прошли викторину!\n\nКол-во баллов: {points}\nРезультат сохранён.", "Ура!", MessageBoxButton.OK, MessageBoxImage.Information);
+                NavigationService.Navigate(new MainPage());
+            }
+        }
+
+        private void ContinueMethod()
+        {
+            textNumberQuestion++;
+            numberQuestion++;
+            answersOffset += 4;
+            maxCount--;
+
+            answerButton.Visibility = Visibility.Visible;
+            continueButton.Visibility = Visibility.Hidden;
+
+            questionNumberTextBox.Text = $"Вопрос #{numberQuestion}";
+            questionTextBox.Text = settings.gameQuestions.ElementAt(textNumberQuestion).Value;
+
+            for (int i = 0; i < radioButtons.Count; i++)
+            {
+                radioButtons[i].IsChecked = false;
+                radioButtons[i].Background = Brushes.White;
+                radioButtons[i].Foreground = Brushes.White;
+                radioButtons[i].Content = settings.gameAnswers.ElementAt(i + answersOffset);
+            }
+
+            progressBar.Value++;
+            StartTimer();
+        }
+
+        private void ContinueButtonClick(object sender, RoutedEventArgs e)
+        {
+            ContinueMethod();
+        }
+
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick -= Timer_Tick;
+            timer.Tick += Timer_Tick;
+            secondsElapsed = settings.MaxTime;
+            UpdateTimeText();
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            secondsElapsed--;
+
+            if (secondsElapsed < 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Вы не успели!");
+                ContinueMethod();
+            }
+            else
+            {
+                UpdateTimeText();
+            }
+        }
+
+        private void UpdateTimeText()
+        {
+            timeTextBox.Text = $"Осталось времени: {secondsElapsed} сек.";
+        }
+
+        private void SaveResult()
+        {
+            SessionCheckUtilities utilities = new SessionCheckUtilities();
+            string uid = utilities.ReadUID();
+
+            SqlConnection connection = new SqlConnection(utilities.ConnectionString);
+            connection.Open();
+
+            string query = "UPDATE Users SET mixed_result = mixed_result + @Result WHERE id = @UID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@Result", points);
+            command.Parameters.AddWithValue("@UID", uid);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
